fb08e615af37764340729ca64d02797faf578a5d
Rithish_25 <rithish0637@gmail.com>
2025-05-16 19:31:05 +0530

    portfolio

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,4 +1,5 @@
 import HeroImg from '../assets/hero.png';
+import RithishImg from '../assets/rithish.jpg';
 import { AiOutlineTwitter, AiOutlineFacebook, AiOutlineLinkedin } from "react-icons/ai";
 
 export default function Hero() {
@@ -11,18 +12,26 @@ export default function Hero() {
         }
     }
 
-
-    return <section className='flex flex-col md:flex-row px-5 py-32 bg-primary justify-center'>
-       <div className='md:w-1/2 flex flex-col'>
-            <h1 className=' text-white text-6xl font-hero-font'>Hi, <br/> Im <span className='text-black'>R</span> Rithish
-                <p className='text-2xl'>{config.subtitle}</p>
-            </h1>
-            <div className='flex py-10'>
-                <a href={config.social.twitter} className='pr-5 hover:text-white'><AiOutlineTwitter size={40} /></a>
-                <a href={config.social.facebook} className='pr-5 hover:text-white'><AiOutlineFacebook size={40}/></a>
-                <a href={config.social.linkedin} className='hover:text-white'><AiOutlineLinkedin size={40}/></a>
+    return (
+        <section className='flex flex-col md:flex-row px-5 py-32 bg-primary justify-center'>
+            <div className='md:w-1/2 flex flex-col'>
+                <h1 className=' text-white text-6xl font-hero-font'>
+                    Hi, <br/> Im <span className='text-black'>R</span> Rithish
+                    <p className='text-2xl'>{config.subtitle}</p>
+                </h1>
+                <div className='flex py-10'>
+                    <a href={config.social.twitter} className='pr-5 hover:text-white'><AiOutlineTwitter size={40} /></a>
+                    <a href={config.social.facebook} className='pr-5 hover:text-white'><AiOutlineFacebook size={40}/></a>
+                    <a href={config.social.linkedin} className='hover:text-white'><AiOutlineLinkedin size={40}/></a>
+                </div>
+                {/* Mobile-only BIG profile image */}
+                <img 
+                    className='block md:hidden w-52 h-52 rounded-full mx-auto mt-5 border-4 border-white shadow-lg' 
+                    src={RithishImg} 
+                    alt="Rithish" 
+                />
             </div>
-       </div>
-       <img className='md:w-1/3' src={HeroImg} />
-    </section>
+            <img className='hidden md:block md:w-1/3' src={HeroImg} alt="Hero Illustration" />
+        </section>
+    );
 }
