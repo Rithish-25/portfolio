c21bda589c8abb98615eb771f11b2d2d6b9cd5d4
Rithish_25 <rithish0637@gmail.com>
2025-12-27 20:44:40 +0530

    resume update

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -3,7 +3,7 @@ import { motion } from 'framer-motion';
 
 export default function Resume () {
     const config = {
-        link : 'https://drive.google.com/file/d/1S6NV2Rmtb8Rn3ry22N69WI_RbGXuDMQe/view?usp=drive_link'
+        link : '/Rithish_Resume.pdf'
     }
 
     return <section id = 'resume' className='flex flex-col md:flex-row bg-secondary px-5'>
@@ -23,7 +23,7 @@ export default function Resume () {
         >
             <div className='flex flex-col justify-center text-white'>
                 <h1 className='text-4xl border-b-4 border-primary mb-5 w-[140px] font-bold'>Resume</h1>
-                <p className='pb-5'>You can view my resume <a className='btn' href={config.link} >Download</a></p>    
+                <p className='pb-5'>You can view my resume <a className='btn' href={config.link} download="Rithish_Resume.pdf">Download</a></p>    
             </div>
         </motion.div>
     </section>
